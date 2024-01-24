fc19d4678f2737106d43d56707ffd7d5671e188c
Callen Shaeffer <callenshaeffer@gmail.com>
2024-01-23 19:02:22 -0500

    reset
    reset

--- food-waste-tracker/Queries/Acquire_Userid.sql
+++
@@ -1,6 +0,0 @@
-
-SELECT userid FROM UserCredentials
-WHERE email = 'diam.vel.arcu@hotmail.edu'
-
-SELECT userid FROM UserCredentials
-WHERE username = 'Bevis'
--- food-waste-tracker/Queries/Locale.sql
+++
@@ -1,9 +0,0 @@
---@block
-/*Display user zipcode*/
-SELECT zipcode FROM HouseholdInfo
-WHERE userid = unique_userid;
-
---@block
-/*Can we store this users data?*/
-SELECT collect_opt FROM HouseholdInfo
-WHERE userid = unique_userid

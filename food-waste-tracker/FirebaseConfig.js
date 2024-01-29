73a1fac736b7de5d4b1c7138673cca5c945e0ba6
Christian Silva <christiansilvaprof@gmail.com>
2024-01-28 21:43:34 -0500

    Fixed the configs to remove most of the startup warnings.

--- food-waste-tracker/FirebaseConfig.js
+++ food-waste-tracker/FirebaseConfig.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
 import { getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -13,11 +14,11 @@ const firebaseConfig = {
   storageBucket: "waste-watcher-b0ea4.appspot.com",
   messagingSenderId: "108888485056",
   appId: "1:108888485056:web:765723fce89a51fe961125",
-  measurementId: "G-Q6WYJJ3QSY"
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-const analytics = getAnalytics(FIREBASE_APP);

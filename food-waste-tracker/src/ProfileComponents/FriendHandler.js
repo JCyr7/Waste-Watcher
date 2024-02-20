5b95437693485e5caf1a5ab3f0755691a840dc62
Christian Silva <christiansilvaprof@gmail.com>
2024-02-20 15:07:18 -0500

    added final friend request functionality

--- food-waste-tracker/src/ProfileComponents/FriendHandler.js
+++ food-waste-tracker/src/ProfileComponents/FriendHandler.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component } from 'react';
-import { addDoc, collection, query, where, getDoc, getDocs, or, and, doc } from "firebase/firestore";
+import { addDoc, collection, query, where, getDoc, getDocs, or, and, doc, setDoc } from "firebase/firestore";
 
 import { FIREBASE_AUTH, FIREBASE_DB } from '../../FirebaseConfig';
 import { Alert } from 'react-native';
@@ -31,6 +31,8 @@ const sortStringsAlphabetically = (s1, s2) => {
     let u1 = "";
     let u2 = "";
 
+    console.log(s1, s2);
+
     // sorts the strings alphabetically
     if (s1.localeCompare(s2) < 0) {
         u1 = s1;
@@ -43,7 +45,7 @@ const sortStringsAlphabetically = (s1, s2) => {
     return [u1, u2];
 }
 
-export const checkFriendRequestStatus = async (user1, user2) => {    
+export const getFriendRequestStatus = async (user1, user2) => {    
 
     if (!user1 || !user2) {
         return null;
@@ -77,7 +79,7 @@ export const sendFriendRequest = async (sender, reciever) => {
     const user1 = userTuple[0];
     const user2 = userTuple[1];
 
-    if (await checkFriendRequestStatus(user1, user2)) {
+    if (await getFriendRequestStatus(user1, user2)) {
         return false;
     }
 
@@ -149,5 +151,53 @@ export const getNameFromID = async (ID) => {
         console.log(e.message);
         return null;
     }
+
+}
+
+export const acceptFriendRequest = async (username1, username2) => {
+
+    if (!username1 || !username2) {
+        return null;
+    }
+
+    username1 = await getUserID(username1);
+    username2 = await getUserID(username2);
+
+    console.log(username1, username2);
+
+    const userTuple = sortStringsAlphabetically(username1, username2);
+
+    const user1 = userTuple[0];
+    const user2 = userTuple[1];
+
+    try {
+
+        const requestRef = collection(FIREBASE_DB, "friendship-matrix");
+        const requestQuery = query(requestRef, where("friend1-ID", "==", user1), where("friend2-ID", "==", user2));
+
+        const requestQuerySnapshot = await getDocs(requestQuery);
+
+        const friendRequestStatus = requestQuerySnapshot.docs[0].data().status;
+    
+        if (!friendRequestStatus || friendRequestStatus == "accepted" || friendRequestStatus == "blocked") {
+            return false;
+        } else if (friendRequestStatus == "pending") {
+
+            const docRef = doc(FIREBASE_DB, "friendship-matrix", requestQuerySnapshot.docs[0].id);
+
+            setDoc(docRef, {
+                status: "accepted"
+            }, {merge: true});
+
+            return true;
+        }
+
+    } catch (e) {
+        console.log("failed");
+        console.log(e.message);
+        return null;
+    }   
+
+    return null;
 
 }

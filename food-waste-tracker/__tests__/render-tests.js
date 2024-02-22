0e74a007bf974f75417a97deb011e9825fe0e1ac
JCyr7 <49315568+JCyr7@users.noreply.github.com>
2024-02-22 13:21:32 -0500

    all tests work + removed alignText errors

--- food-waste-tracker/__tests__/render-tests.js
+++ food-waste-tracker/__tests__/render-tests.js
@@ -6,7 +6,6 @@ import ArcGISMap from '../src/Pages/ArcGISMap'
 import ForgotPassword from '../src/Pages/ForgotPassword'
 import HomePage from '../src/Pages/HomePage'
 import LeaderBoardPage from '../src/Pages/LeaderboardPage'
-//import Login from '../src/Pages/LoginPage'
 import LogoutPage from '../src/Pages/LogoutPage'
 import ProfilePage from '../src/Pages/ProfilePage'
 
@@ -22,39 +21,28 @@ test('renders correctly', () => {
   expect(tree).toMatchSnapshot()
 })
 
-/*test('renders correctly', () => {
+test('renders correctly', () => {
   const tree = renderer.create(<HomePage />).toJSON()
   expect(tree).toMatchSnapshot()
-})*/
+})
 
-/*test('renders correctly', () => {
+test('renders correctly', () => {
   const tree = renderer.create(<LeaderBoardPage />).toJSON()
   expect(tree).toMatchSnapshot()
-})*/
+})
 
-/*
+const mockObject = {
+  getItem: jest.fn()
+};
+
 describe('HomePage', () => {
   it('calls getItem on the object', () => {
     // Render the component
     renderer.create(<HomePage object={mockObject} />);
-
-    // Check that getItem was called
+    
     expect(mockObject.getItem).toMatchSnapshot();
-  }); // Add closing parenthesis here
-});/*
-
-
-
-
-/*test('renders correctly', () => {
-  const tree = renderer.create(<Login />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-/*test('renders correctly', () => {
-  const tree = renderer.create(<ArcGISMap />).toJSON()
-  expect(tree).toMatchSnapshot()
-})*/
+  }); 
+});
 
 test('renders correctly', () => {
   const tree = renderer.create(<ProfilePage />).toJSON()
